49e87dccf5cce3db5a0179244eaa3277ebae06db
gonzo1776 <mgonzo1785@gmail.com>
2019-07-21 02:49:42 +0000

    add a strikethrough

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,26 +1,12 @@
-<section class="todoapp">
-  <header class="header">
-    <h1>To-Do's</h1>
-    <form id="new-form">
-      <input type="text" class="new-todo" placeholder="What needs to be done?" />
-    </form>
-  </header>
-
-  <section class="main">
-    <ul class="todo-list">
-
-    </ul>
-  </section>
-</section>
-
-<script>
-  $(function() {
+$(function() {
     // The taskHtml method takes in a JavaScript representation
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -37,16 +23,19 @@
     function toggleTask(e) {
       var itemId = $(e.target).data("id");
 
-
       var doneValue = Boolean($(e.target).is(':checked'));
-
 
       $.post("/tasks/" + itemId, {
         _method: "PUT",
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -61,11 +50,23 @@
       $('.toggle').change(toggleTask);
 
     });
-  });
-</script>
 
-<section class="author">
-  A <%= link_to "todo-mvc", "http://todo.mvc.com" %>
-  application built by
-  <%= link_to "Ken Mazaika", "http://github.com/kenmazaika" %>
-</section>
+
+    $('#new-form').submit(function(event) {
+      event.preventDefault();
+      var textbox = $('.new-todo');
+      var payload = {
+        task: {
+          title: textbox.val()
+        }
+      };
+      $.post("/tasks", payload).success(function(data) {
+        var htmlString = taskHtml(data);
+        var ulTodos = $('.todo-list');
+        ulTodos.append(htmlString);
+        $('.toggle').click(toggleTask);
+        $('.new-todo').val('');
+      });
+    });
+
+  });
